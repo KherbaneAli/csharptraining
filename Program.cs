4681b2cb489eaf7d474596d1279dea97b3ef557b
KherbaneAli <46484952+KherbaneAli@users.noreply.github.com>
2019-01-21 14:37:44 +0000

    reading csv and printing

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace csharptraining
 {
@@ -6,7 +12,27 @@ namespace csharptraining
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            File.WriteAllLines("treesnew.csv",File.ReadAllLines(@"\_git\csharptraining\data\trees.csv").Where(l => !string.IsNullOrWhiteSpace(l)));
+            
+            var treeData = File.ReadAllLines(@"\_git\csharptraining\treesnew.csv");
+        
+            var treeList = from treeInfo in treeData
+                    let data = treeInfo.Split(',')
+                    select new
+                    {
+                        Index = data[0],
+                        Girth = data[1],
+                        Height= data[2],
+                        Volume = data[3],
+                    };
+                    
+            foreach (var tree in treeList)
+            {
+                Console.WriteLine("|" + tree.Index + "|" + tree.Girth + "|" + tree.Height + "|" + tree.Volume + "|");
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
         }
     }
 }
